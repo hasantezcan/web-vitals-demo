e57d764c4206d117f3326e4d1c99afa49f4a1c6e
Hasan Tezcan <hasantezcan77@gmail.com>
2023-05-05 16:34:15 +0300

    chore: update api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://web-vitals-demo.onrender.com/api";
+export const API_URL = "https://web-vitals-demo.onrender.com/api";
 export const PLACEHOLDER =
   "https://cdn.dsmcdn.com/sfint/production/image-placeholder_1667421936752.jpg";
